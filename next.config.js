d671ad9f43d6a01fcc9b4c7c1d7bc51e13cb8844
yiyaang <yiyaaang@gmail.com>
2024-11-05 01:23:51 +0900

    fix: Remove static export and update WooCommerce API keys

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // 프로덕션 빌드 시 ESLint 검사를 비활성화
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // 프로덕션 빌드 시 타입 검사를 비활성화
     ignoreBuildErrors: true,
   },
-  output: 'export',
   images: {
     unoptimized: true,
   },
